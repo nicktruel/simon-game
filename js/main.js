65e9b84a5260f719709e8700e70f5ac95dfbe690
Nick Truel <nick.truel@outlook.com>
2018-12-06 12:39:01 +0000

    added effects to buttons and change bg image

---
+++ js/main.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+    
+/*  $("#Green").click(function(){
+        $(this).css("background-image", "linear-gradient(#7aff00, #defdad)");
+    });
+    
+     
+    $("#Red").click(function(){
+        $(this).css("background-image", "linear-gradient(#ff4949, #ffbdbd)");
+    });
+    
+     
+    $("#Yellow").click(function(){
+        $(this).css("background-image", "linear-gradient(#fffdba, #ffff43)");
+    });
+    
+     
+    $("#Blue").click(function(){
+        $(this).css("background-image", "linear-gradient(#caffee, #1E90FF)");
+    }); 
+*/
+
+    $("#startButton").click(function(){
+        $(".machine").removeClass("Blue");
+        $(".machine").addClass("pressStart");
+    })
+
+})
+
+
