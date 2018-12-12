841703ce3ac0b0189ad1074f732f5ca008be2812
Nick Truel <nick.truel@outlook.com>
2018-12-12 07:06:40 +0000

    added function when start button is pressed and cleaned up code, created functions for when buttons activated

--- js/main.js
+++ js/main.js
@@ -1,85 +1,119 @@
 $(document).ready(function(){
 
- $(function(){
-    $(".green").click(function(){
-        greenSound.play();
-        $(".green").addClass("pressGreen");
-        setTimeout(eraseClass, 150);
-    });
-        function eraseClass(){
-            $(".green").removeClass("pressGreen");
-        }
- });
+    var greenSound = document.createElement("audio");
+        greenSound.src = "sounds/simonSound1.mp3";
+
+    var redSound = document.createElement("audio");
+        redSound.src = "sounds/simonSound2.mp3";
+
+    var yellowSound = document.createElement("audio");
+        yellowSound.src = "sounds/simonSound3.mp3";
     
- $(function(){
-    $(".red").click(function(){
-        redSound.play();
-        $(".red").addClass("pressRed");
-        setTimeout(eraseClass, 150);
-    });
-        function eraseClass(){
-            $(".red").removeClass("pressRed");
-        }
- });
- 
- $(function(){
-    $(".yellow").click(function(){
-        yellowSound.play();
-        $(".yellow").addClass("pressYellow");
-        setTimeout(eraseClass, 150);
-    });
-        function eraseClass(){
-            $(".yellow").removeClass("pressYellow");
-        }
- });
- 
- $(function(){
-    $(".blue").click(function(){
-        blueSound.play();
-        $(".blue").addClass("pressBlue");
-        setTimeout(eraseClass, 150);
-    });
-        function eraseClass(){
-            $(".blue").removeClass("pressBlue");
-        }
- });
+    var blueSound = document.createElement("audio");
+        blueSound.src = "sounds/simonSound4.mp3";
+      
+    var startSound = document.createElement("audio");
+        startSound.src = "sounds/startSound.mp3";
 
-var greenSound = document.createElement("audio");
-  greenSound.src = "sounds/simonSound1.mp3";
-  greenSound.volume = 0.2;
+// functions for 4 color buttons activated 
 
-var redSound = document.createElement("audio");
-  redSound.src = "sounds/simonSound2.mp3";
-  redSound.volume = 0.2;
+    function activateGreen() {
+         $(".green").addClass("pressGreen"), greenSound.play();
+         setTimeout(function eraseClass() {$(".green").removeClass("pressGreen")}, 150);
+    } 
+    
+    function activateRed() {
+         $(".red").addClass("pressRed"), redSound.play();
+         setTimeout(function eraseClass() {$(".red").removeClass("pressRed")}, 150);
+    }
+    
+    function activateYellow() {
+         $(".yellow").addClass("pressYellow"), yellowSound.play();
+         setTimeout(function eraseClass() {$(".yellow").removeClass("pressYellow")}, 150);
+    }  
+    
+    function activateBlue() {
+         $(".blue").addClass("pressBlue"), blueSound.play();
+         setTimeout(function eraseClass() {$(".blue").removeClass("pressBlue")}, 150);
+    } 
 
-var yellowSound = document.createElement("audio");
-  yellowSound.src = "sounds/simonSound3.mp3";
-  yellowSound.volume = 0.2;
+// When color buttons are pressed
 
-var blueSound =document.createElement("audio");
-  blueSound.src = "sounds/simonSound4.mp3";
-  blueSound.volume = 0.2;
-  
-  
-  
-
-/*
- var buttons = $(".green", ".red", ".yellow", ".blue");
- 
- buttons.each(function(){
-     var thisButtons = $(this);
-     thisButtons.click(function(){
-         thisButtons.addClass("pressBlue");
-         setTimeout(eraseClass, 150);
+ $(function() {
+     $(".green").mousedown(function() {
+         activateGreen();
      });
-        function eraseClass(){
-            thisButtons.removeClass("pressBlue");
-        }
  });
- */
+
+ $(function() {
+     $(".red").mousedown(function() {
+         activateRed();
+     });
+ });
+
+ $(function() {
+     $(".yellow").mousedown(function() {
+        activateYellow();
+     });
+ });
+
+ $(function() {
+     $(".blue").mousedown(function() {
+        activateBlue();
+     });
+ });
+
+
+// When start button is pressed
  
+ $(function(pressStartButton) {
+    $("#star-reset").mousedown(function() {
+
+        lightGreenButton();
+        lightRedButton();
+        lightYellowButton();
+        lightBlueButton();
+
+        startSound.play();
+
+        function lightGreenButton() {
+            activateGreen(), greenSound.pause();
+        }
+
+        function lightRedButton() {
+            activateRed(), redSound.pause();
+        }
+
+        function lightYellowButton() {
+           activateYellow(), yellowSound.pause();
+        }
+
+        function lightBlueButton() {
+           activateBlue(), blueSound.pause();
+        }
+    });
+    
+ // Random suite
  
-  
+    var suite = [1, 2, 3, 2, 0, 1, 1, 2, 3, 0];
+    var numberOfRound = 5;
+    
+    startSuite();
+    
+    function startSuite() {
+        for (var i = 0; i < numberOfRound; i++) {
+            var actualLight = suite[i];
+            if (actualLight === 0) activateGreen();
+            if (actualLight === 1) activateRed();
+            if (actualLight === 2) activateYellow();
+            if (actualLight === 3) activateBlue();
+        }
+    }
+    
+ });
+   
+
+
 
 });
 
